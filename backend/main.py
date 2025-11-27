d0d195ee9cf2fce580a96903140f9aec18a9fa32
xevansz <minithbmatthew@gmail.com>
2025-11-27 20:23:01 +0530

    docker is over; mm

--- backend/main.py
+++ backend/main.py
@@ -64,7 +64,7 @@ async def root():
     }
 
 # build files
-app.mount("/", StaticFiles(directory="frontend/build", html=True), name="frontend")
+app.mount("/", StaticFiles(directory="build", html=True), name="frontend")
 
 # Startup event
 @app.on_event("startup")
